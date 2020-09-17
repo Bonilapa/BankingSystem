f577d3eeb10f703d2447bb775c7c105dc25bbebd
bonilapa <a.astakhov@innopolis.ru>
2020-09-17 19:39:09 +0300

    delete Loyality testing

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ public class Main {
         Client client1 = new Client(LoyaltyLvl.Regular);
         Client client2 = new Client(LoyaltyLvl.Golden);
         Client client3 = new Client(LoyaltyLvl.VIP);
-        System.out.println(client1.loyaltyLvl.getFeeNumerucValue());
+        System.out.println(client1.loyaltyLvl.getFeeNumericValue());
         System.out.println(client1.loyaltyLvl.getFeePercentValue());
-        System.out.println(client2.loyaltyLvl.getFeeNumerucValue());
-        System.out.println(client2.loyaltyLvl.getFeePercentValue());
-        System.out.println(client3.loyaltyLvl.getFeeNumerucValue());
-        System.out.println(client3.loyaltyLvl.getFeePercentValue());
     }
 }
